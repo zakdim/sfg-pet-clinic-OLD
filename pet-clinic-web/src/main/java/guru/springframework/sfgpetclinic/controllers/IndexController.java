8b16a8986de39ef0dc36b6cc84ad324bec596827
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2020-08-13 22:36:04 -0400

    S04-L64 Create Index Page and Controller
    Closes #8

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
@@ -0,0 +1,17 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by dmitri on 2020-08-13.
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String index() {
+
+        return "index";
+    }
+}
