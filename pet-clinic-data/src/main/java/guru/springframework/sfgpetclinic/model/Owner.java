5b70da61b5dd1a07951ae556f104df07ab8f63d3
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2020-08-27 23:26:29 -0400

    S07-118 Create Pet Type, Pet and Visit Entities
    Add visits. Update owner with pet relationship.
    Closes #27

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,7 +1,19 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  * Created by dmitri on 2020-08-09.
  */
 public class Owner extends Person {
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Visit.java
@@ -0,0 +1,37 @@
+package guru.springframework.sfgpetclinic.model;
+
+import java.time.LocalDate;
+
+/**
+ * Created by dmitri on 2020-08-27.
+ */
+public class Visit extends BaseEntity {
+
+    private LocalDate date;
+    private String description;
+    private Pet pet;
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+}
