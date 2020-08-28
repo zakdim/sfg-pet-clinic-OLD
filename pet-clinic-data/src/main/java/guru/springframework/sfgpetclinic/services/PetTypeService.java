aa77612cc427a75f4f404f83751af74be9dd6b1e
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2020-08-28 12:47:36 -0400

    S07-121 Create Pet Type Map Service
    Closes #30

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,9 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.PetType;
+
+/**
+ * Created by dmitri on 2020-08-28.
+ */
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
