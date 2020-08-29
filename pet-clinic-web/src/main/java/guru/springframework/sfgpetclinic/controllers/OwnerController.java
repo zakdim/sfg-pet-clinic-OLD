6778b34fbd88b8c7aaeb03038fc416ff7126f216
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2020-08-28 23:22:19 -0400

    S07-126 Fixing Broker Links
    Closes #46

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -25,4 +25,9 @@ public class OwnerController {
 
         return "owners/index";
     }
+
+    @RequestMapping({"/find"})
+    public String findOwners() {
+        return "notimplemented";
+    }
 }
