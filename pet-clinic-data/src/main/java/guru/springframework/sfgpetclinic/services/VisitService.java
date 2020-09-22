3e1333f8e30f4797fb803b995fe405cbc25dbfec
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2020-09-21 23:07:38 -0400

    S08-156 Add Map Based Visit Service
    Closes #47

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,10 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.Visit;
+
+/**
+ * Created by dmitri on 2020-09-21.
+ */
+public interface VisitService extends CrudService<Visit, Long> {
+    
+}
